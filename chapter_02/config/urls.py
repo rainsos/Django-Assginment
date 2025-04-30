6e92d4ee5c26473eafe2b15ed87b1b0df41330e2
Chansman <rainsos@nate.com>
2025-05-01 01:21:14 +0900

    ✨ Feat: chapter_02 과제 업로드
    Django project

--- chapter_02/config/urls.py
+++ chapter_02/config/urls.py
@@ -1,22 +1,14 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path  #
+from todo.views import todo_list, todo_info, todo_create, todo_update, todo_delete, todo_toggle
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('todo/', todo_list, name='todo_list'),
+    path('todo/<int:todo_id>/', todo_info, name='todo_info'),
+    path('todo/create/', todo_create, name='todo_create'),
+    path('todo/<int:todo_id>/edit/', todo_update, name='todo_update'),
+    path('todo/<int:todo_id>/delete/', todo_delete, name='todo_delete'),
+    path('todo/<int:todo_id>/toggle/', todo_toggle, name='todo_toggle'),
 ]
+
